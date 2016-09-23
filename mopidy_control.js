38bfc25c0afc5405adfefa69969df2564a848f1a
Jose Luis Espinoza <psyrax@Joses-MacBook-Pro.local>
2016-09-23 13:00:30 -0500

    sync

---
+++ mopidy_control.js
@@ -0,0 +1,15 @@
+/* Config options */
+var config = require("./config.json");
+var Mopidy = require("mopidy");
+
+var mopidy = new Mopidy({
+    webSocketUrl: config.mopidyURL,
+    callingConvention: "by-position-or-by-name"
+});
+
+mopidy.on("state:online", function () {
+    mopidyConnected = true;
+    mopidy.playback.getCurrentTlTrack().then(function(data){
+  		console.log(data);
+	});
+});
